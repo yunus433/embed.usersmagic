0a20c5aca24534377fbf17dd53a15c01cc8fee1b
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-04-09 00:03:55 +0300

    Delete unused codes

--- models/template/Template.js
+++ models/template/Template.js
@@ -4,7 +4,7 @@ const validator = require('validator');
 
 const getTemplate = require('./functions/getTemplate');
 
-const type_values = ['demographics', 'brand', 'product']; // For now, all templates will be demographics
+const type_values = ['demographics', 'brand', 'product'];
 const subtype_values = ['yes_no', 'single', 'multiple', 'list', 'time', 'scale', 'number'];
 const language_values = ['en', 'tr'];
 
@@ -102,93 +102,6 @@ TemplateSchema.statics.findTemplateByIdAndFormat = function (id, callback) {
       return callback(null, template);
     });
   });
-};
-
-TemplateSchema.statics.createTemplate = function (data, callback) {
-  const Template = this;
-
-  let choices = [];
-  let min_value = null;
-  let max_value = null;
-  let labels = {
-    left: null,
-    middle: null,
-    right: null
-  };
-  let timeout_duration_in_week_by_choices = null;
-
-  if (!data.type || !type_values.includes(data.type))
-    return callback('bad_request');
-
-  if (!data.timeout_duration_in_week || !Number.isInteger(data.timeout_duration_in_week))
-    return callback('bad_request');
-
-  if (!data.name || !data.text || !data.type || !data.subtype)
-    return callback('bad_request');
-
-  if (typeof data.name != 'string' || !data.name.trim().length || data.name.trim().length > MAX_DATABASE_TEXT_FIELD_LENGTH)
-    return callback('bad_request');
-
-  if (typeof data.text != 'string' || !data.text.trim().length || data.text.trim().length > MAX_DATABASE_TEXT_FIELD_LENGTH)
-    return callback('bad_request');
-
-  if (!subtype_values.includes(data.subtype))
-    return callback('bad_request');
-
-  if (data.subtype == 'single' || data.subtype == 'multiple' || data.subtype == 'list' || data.subtype == 'time') {
-    choices = data.choices;
-
-    if (!choices || !Array.isArray(choices) || choices.length > MAX_DATABASE_ARRAY_FIELD_LENGTH || !choices.length || choices.find(each => each.length > MAX_DATABASE_TEXT_FIELD_LENGTH))
-      return callback('bad_request');
-
-    if (data.subtype == 'time' && data.timeout_duration_in_week_by_choices)
-      timeout_duration_in_week_by_choices = data.timeout_duration_in_week_by_choices;
-  }
-
-  if (data.subtype == 'scale' || data.subtype == 'number') {
-    min_value = data.min_value;
-    max_value = data.max_value;
-
-    if (!Number.isInteger(min_value) || !Number.isInteger(max_value) || min_value > max_value)
-      return callback('bad_request');
-  }
-
-  if (data.subtype == 'scale') {
-    if (data.labels && data.labels.left && typeof data.labels.left == 'string' && data.labels.left.length && data.labels.left.length < MAX_DATABASE_TEXT_FIELD_LENGTH)
-      labels.left = data.labels.left;
-    if (data.labels && data.labels.middle && typeof data.labels.middle == 'string' && data.labels.middle.length && data.labels.middle.length < MAX_DATABASE_TEXT_FIELD_LENGTH)
-      labels.middle = data.labels.middle;
-    if (data.labels && data.labels.right && typeof data.labels.right == 'string' && data.labels.right.length && data.labels.right.length < MAX_DATABASE_TEXT_FIELD_LENGTH)
-      labels.right = data.labels.right;
-  }
-
-  Template
-    .find()
-    .countDocuments()
-    .then(order_number => {
-      const newTemplateData = {
-        timeout_duration_in_week: data.timeout_duration_in_week,
-        order_number,
-        language: data.language && language_values.includes(data.language) ? data.language : DEFAULT_LANGUAGE_VALUE,
-        name: data.name.trim(),
-        text: data.text.trim(),
-        type: data.type,
-        subtype: data.subtype,
-        choices: choices,
-        min_value: min_value,
-        max_value: max_value,
-        labels: labels
-      };
-
-      const newTemplate = new Template(newTemplateData);
-
-      newTemplate.save((err, template) => {
-        if (err) return callback('database_error');
-
-        return callback(null, template._id.toString());
-      });
-    })
-    .catch(err => callback('database_error'));
 };
 
 TemplateSchema.statics.findTemplatesByFiltersAndSorted = function (data, callback) {
